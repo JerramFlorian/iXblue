240d2ed1d17196175bcc8cf007c41d80146eb693
Augustin Morge <augustin.morge@ixblue.com>
2023-03-16 17:24:05 +0100

    add lissajou curb and guidance

--- Kalman/kalman_aug.py
+++ Kalman/kalman_aug.py
@@ -15,19 +15,58 @@ def f(X, u):
                       [u2*np.sin(θ) - u3*np.cos(θ)]])
     return x_dot
 
-#List of waypoints
-# Wps = 10*array([[1,-1,0],#,0,15,30,15],
-#               [0,0,1]])#,25,30,15,20]])
+sawtooth = lambda x : (2*np.arctan(np.tan(x/2)))
 
-Wps = array([[0,15,30,15],
-              [25,30,15,20]])
+def control(x,t):
+    x1, x2, x3, x4, x5 = x.flatten()
+    L = 15
+    K = 0.5
+    f = 0.1
+    nb = 2 
 
-a = -500
-b = 500
-N = 500
+    w=L*array([[cos(f*t)],[sin(f*nb*t)]])  
+    dw=L*array([[-f*sin(f*t)],[nb*f*cos(f*nb*t)]])  
+    ddw=L*array([[-f**2*cos(f*t)],[-nb**2*f**2*sin(f*nb*t)]])
+    
+    Ax = array([[cos(x3), sin(x3)],
+                [sin(x3), -cos(x3)]])
+    
+    y = array([[x1],
+               [x2]])
+    
+    dy = array([[x4],
+                [x5]])
+    
+    ddy = w-y + 2*(dw-dy) #+ ddw
+    
+    u = inv(Ax)@ddy
+    u1 = -K*sawtooth(x3 - np.arctan2(w[1,0]-x2,w[0,0] - x1))
+    print(u)
+    u = np.vstack((u1,u))
 
-Wps = np.random.uniform(low=a, high=b, size=(2, N))
-print(Wps)
+    print(u,'\n')
+    if np.linalg.norm(u) > 500:
+        sys.exit()
+
+    scatter(w[0,0],w[1,0], color = 'red', label = 'point to follow')
+    W = lambda f : np.array([L*cos(f*t), L*sin(nb*f*t)])
+    scatter(W(np.linspace(0,100,10000))[0],W(np.linspace(0,100,10000))[1], s = 0.1)
+
+    return u 
+
+# List of waypoints
+
+Wps = 10*array([[0],#,0,15,30,15],
+              [1]])#,25,30,15,20]])
+
+# Wps = array([[0,15,30,15],
+#               [25,30,15,20]])
+
+# a = -20
+# b = 20
+# N = 5
+# Wps = np.random.uniform(low=a, high=b, size=(2, N))
+
 # Observation function
 def g(x):
     x=x.flatten()
@@ -53,7 +92,7 @@ def g(x):
             Beta.append(0.0001)
     Γβ = diag(Beta)
     if len(Beta) != 0:
-        y = y #+ mvnrnd1(Γβ)
+        y = y + mvnrnd1(Γβ)
     return H, y, Γβ, wp_detected
 
 def Kalman(xbar, P, u, y, Q, R, F, G, H):
@@ -130,19 +169,19 @@ if __name__ == "__main__":
     GNSS = False
 
     if display_bot:
-        ax = init_figure(-60, 60, -60, 60)
+        ax = init_figure(-20, 20, -20, 20)
     T = []
 
     import sys
     N = 1000
     PMatrix = np.zeros((N,25))
 
-    P = 0.01 * np.eye(5)
+    P = 10 * np.eye(5)
     X = np.array([[0], [0], [0], [0], [0]])
     Xhat = X
 
     sigm_equation = 0.1
-    sigm_measure = 1
+    sigm_measure = 0.1
     Q = np.diag([sigm_equation, sigm_equation, sigm_equation])
     R = np.diag([sigm_measure, sigm_measure])
 
@@ -150,10 +189,11 @@ if __name__ == "__main__":
     u = np.array([[0.1], [0.2], [-1]])
 
     for i in np.arange(0, N*dt, dt):
-
+        
         # Real state
+        u = control(X,i)
         X = X + dt*f(X,u)
-
+        
         u1, u2, u3 = u.flatten()
         x, y, θ, vx, vy = X.flatten()
 
@@ -184,25 +224,24 @@ if __name__ == "__main__":
             if wp_detected:
                 Xhat, P, ytilde = Kalman(Xhat, P, u, Y, Q, R, Fk, Gk, Hk)
             else:
-                Xhat = Xhat + dt*f(Xhat,u) #+ mvnrnd1(Gk @ Q @ Gk.T) #Fk @ Xhat + Gk @ u
+                Xhat = Xhat + dt*f(Xhat,u) + mvnrnd1(Gk @ Q @ Gk.T) #Fk @ Xhat + Gk @ u
                 P = Fk @ P @ Fk.T + Gk @ Q @ Gk.T
 
         if display_bot:
             # Display the results
-
-            clear(ax)
-            legende(ax)
-
             draw_tank(X)
             draw_ellipse_cov(ax, Xhat[0:2], P[0:2, 0:2], 0.9, Xhat[2,0], col='black')
             ax.scatter(Xhat[0, 0], Xhat[1, 0], color='red', label = 'Estimation of position', s = 5)
             ax.legend()
-            ax.set_xlim(X[0,0]-50, X[0,0]+50)
-            ax.set_ylim(X[1,0]-50, X[1,0]+50)
+            # ax.set_xlim(X[0,0]-21, X[0,0]+21)
+            # ax.set_ylim(X[1,0]-21, X[1,0]+21)
 
             scatter(Wps[0], Wps[1])
 
             pause(0.001)
+
+            clear(ax)
+            legende(ax)
 
         # Append lists to visualize our covariance model
         T.append(i)
